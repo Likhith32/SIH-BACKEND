d1031e212fd0b48a87940b50ceff4a29947d21ff
Likhith32 <likhithmanakala@gmail.com>
2025-09-29 16:40:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,6 +174,7 @@ def predict_csv():
         return send_file(tmp_file.name, as_attachment=True, download_name="predicted_analysis.csv", mimetype="text/csv")
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
+    app.run(debug=True, host="0.0.0.0", port=port)
+
